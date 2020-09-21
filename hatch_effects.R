7df0770a51a42642cd45b76a470cafedb88dae0b
Connors <CONNORSB@ENT.dfo-mpo.ca>
2020-09-21 16:03:46 -0700

    Fixed hatchery "effect" errors
    Revised hatch_effects.R fil to fixed errors associated with calculation of magnitude of hatchery production in most recent decade with data as well as SST anomalies for each ocean region over same time period.

--- hatch_effects.R
+++ hatch_effects.R
@@ -7,10 +7,10 @@ raw_comp <- read.csv(file = "data/competitor_indices_2019_09_26.csv", header = T
 sd_total_pinks <- sd(raw_comp$pink_numbers_np[raw_comp$Year > 1975])
 
 # Average hatchery mean production in recent years as proportion of total pink SDU 
-sdu_perc_hatch_pink <- mean(raw_comp$pink_numbers_np_hatch[raw_comp$Year >= 2005]) / sd_total_pinks
+sdu_perc_hatch_pink <-mean(scale(raw_comp$pink_numbers_np_hatch[raw_comp$Year > 1975], scale = sd_total_pinks)[30:40,1])
 
 # Average total pink production in recent years in SDUs 
-sdu_perc_pink <- mean(scale(raw_comp$pink_numbers_np[raw_comp$Year > 1975])[31:40,1])
+sdu_perc_pink <- mean(scale(raw_comp$pink_numbers_np[raw_comp$Year > 1975])[30:40,1])
 
 # Average wild pink production in recent years in SDUs
 sdu_perc_wild_pink <- sdu_perc_pink-sdu_perc_hatch_pink
@@ -30,34 +30,45 @@ wc_sstxpink_coef <- summary(hb07r2, pars = "mu_chi")$summary[1, "mean"]
 goa_sstxpink_coef <- summary(hb07r2, pars = "mu_chi")$summary[2, "mean"]
 bs_sstxpink_coef <- summary(hb07r2, pars = "mu_chi")$summary[3, "mean"]
 
-# Average SST anomaly last 10 years of time series (2005-2015)
-sdu_WC_sst <- 0.1254754
-sdu_GoA_sst <- 0.3270323
-sdu_BS_sst <- 0.5037103
+# Average SST anomaly over last 10 years of time series (2005-2015); mean by stock and then mean among stocks within same ocean region
+stock_info <- read.csv( "./data/master_stock_info.csv")
+raw.clim <- read.csv(file="data/sst_yr_1_stock_anomalies.csv",header=TRUE)
+stock_raw_sst <- merge(raw.clim,stock_info, by= "Stock.ID")
+
+region_sst_anomalies <- stock_raw_sst %>%
+  group_by(Ocean.Region,Stock)%>%
+  filter(Year > 2004 & Year < 2016)%>%
+  summarise(mean_SST_SDUs = mean(sst_anomaly))%>%
+  group_by(Ocean.Region)%>%
+  summarise(mean_SST_SDUs = mean(mean_SST_SDUs))
+  
+sdu_WC_sst <- region_sst_anomalies[3,2]
+sdu_GoA_sst <- region_sst_anomalies[2,2]
+sdu_BS_sst <- region_sst_anomalies[1,2]
 
 # Recent hatch pink "effect" given recent SST (need to account for bc can modify pink effect via interaction term)
 hatch_pink_perc_wc <- (sdu_perc_hatch_pink * wc_pink_coef)  + (sdu_WC_sst*sdu_perc_hatch_pink*wc_sstxpink_coef)
 hatch_pink_perc_goa <- (sdu_perc_hatch_pink * goa_pink_coef) + (sdu_GoA_sst*sdu_perc_hatch_pink*goa_sstxpink_coef)
 hatch_pink_perc_bs <- (sdu_perc_hatch_pink * bs_pink_coef) + (sdu_BS_sst*sdu_perc_hatch_pink*bs_sstxpink_coef)
 
-(exp(hatch_pink_perc_wc) - 1) * 100
-(exp(hatch_pink_perc_goa) - 1) * 100
-(exp(hatch_pink_perc_bs) - 1) * 100
+round((exp(hatch_pink_perc_wc) - 1) * 100,digits=1)
+round((exp(hatch_pink_perc_goa) - 1) * 100,digits=1)
+round((exp(hatch_pink_perc_bs) - 1) * 100,digits=1)
 
 # % change in productivty given recent total pinks and SST
 total_pink_perc_wc_combined <- (sdu_perc_pink * wc_pink_coef) + (sdu_WC_sst*wc_sst_coef) + (sdu_WC_sst*sdu_perc_pink*wc_sstxpink_coef)
 total_pink_perc_goa_combined <- (sdu_perc_pink * goa_pink_coef) + (sdu_GoA_sst*goa_sst_coef) + (sdu_GoA_sst*sdu_perc_pink*goa_sstxpink_coef)
 total_pink_perc_bs_combined <- (sdu_perc_pink * bs_pink_coef) + (sdu_BS_sst*bs_sst_coef) + (sdu_BS_sst*sdu_perc_pink*bs_sstxpink_coef)
 
-(exp(total_pink_perc_wc_combined) - 1) * 100
-(exp(total_pink_perc_goa_combined) - 1) * 100
-(exp(total_pink_perc_bs_combined) - 1) * 100
+round((exp(total_pink_perc_wc_combined) - 1) * 100,digits=1)
+round((exp(total_pink_perc_goa_combined) - 1) * 100,digits=1)
+round((exp(total_pink_perc_bs_combined) - 1) * 100,digits=1)
 
 # % change in productivty given recent wild pinks and SST
 wild_pink_perc_wc_combined <- (sdu_perc_wild_pink * wc_pink_coef) + (sdu_WC_sst*wc_sst_coef) + (sdu_WC_sst*sdu_perc_wild_pink*wc_sstxpink_coef)
 wild_pink_perc_goa_combined <- (sdu_perc_wild_pink * goa_pink_coef) + (sdu_GoA_sst*goa_sst_coef) + (sdu_GoA_sst*sdu_perc_wild_pink*goa_sstxpink_coef)
 wild_pink_perc_bs_combined <- (sdu_perc_wild_pink * bs_pink_coef) + (sdu_BS_sst*bs_sst_coef) + (sdu_BS_sst*sdu_perc_wild_pink*bs_sstxpink_coef)
 
-(exp(wild_pink_perc_wc_combined) - 1) * 100
-(exp(wild_pink_perc_goa_combined) - 1) * 100
-(exp(wild_pink_perc_bs_combined) - 1) * 100
+round((exp(wild_pink_perc_wc_combined) - 1) * 100,digits=1)
+round((exp(wild_pink_perc_goa_combined) - 1) * 100,digits=1)
+round((exp(wild_pink_perc_bs_combined) - 1) * 100,digits=1)
--- load.R
+++ load.R
@@ -24,6 +24,7 @@ library(codatools)
 library(loo)
 library(rstan)
 library(bayesplot)
+library(tidyverse)
 
 ## Install packages from github
 # library(devtools)
